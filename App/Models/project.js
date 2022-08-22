a0bc21a11e85d3d607318adfc1d33054e0c6d996
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-08-22 15:22:05 +0430

    Create Project Model for Database

---
+++ App/Models/project.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const ProjectSchema = new mongoose.Schema({
+    title : {type : String , required : true },
+    text : {type : String },
+    image : {type : String , default : "/default/default.png"},
+    owner: {type : mongoose.Types.ObjectId , required : true},
+    team : {type : mongoose.Types.ObjectId},
+    private : {type : boolean , default : true},
+},{timestamps : true})
+
+const ProjectModel = mongoose.model('team',ProjectSchema);
+module.exports = {ProjectModel}
