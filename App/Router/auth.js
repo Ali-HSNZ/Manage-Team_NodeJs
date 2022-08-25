50e18da1f5c5a54ca77ff8bbc4ddee75b1b023bb
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-08-25 14:33:34 +0430

    import and use expressValidatorMapper | Replace CheckErrors with expressValidatorMapper => App/Router/auth.js

--- App/Router/auth.js
+++ App/Router/auth.js
@@ -1,9 +1,11 @@
 const { AuthController } = require('../Http/Controllers/auth.controller')
-const { expressValidatorMaper } = require('../Http/Middlewares/checkErrors')
-const { registerValidator } = require('../Http/Validations/auth')
+const { expressValidatorMaper } = require('../Http/Middlewares/expressValidatorMaper')
+const { registerValidator , loginValidation} = require('../Http/Validations/auth')
+
 const Router = require('express').Router()
 
-Router.post('/register' , registerValidator() ,expressValidatorMaper, AuthController.register )
+Router.post('/register' , registerValidator() , expressValidatorMaper , AuthController.register )
+Router.post('/login' , loginValidation() , expressValidatorMaper  , AuthController.login)
 
 module.exports = {
     authRoutes : Router
