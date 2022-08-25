cf59a3d35c4146e7ecd00544ee676a4238e030d3
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-08-25 14:36:23 +0430

    Create '/profile - POST method' for update user profile => App/Router/user.js

--- App/Router/user.js
+++ App/Router/user.js
@@ -1,4 +1,10 @@
+const { UserController } = require('../Http/Controllers/user.controller')
+const {autoLogin} = require('../Http/Middlewares/autoLogin')
+const { expressValidatorMaper } = require('../Http/Middlewares/expressValidatorMaper')
 const Router = require('express').Router()
+
+Router.post('/profile' , autoLogin , UserController.editProfile)
+
 
 module.exports = {
     userRoutes : Router
