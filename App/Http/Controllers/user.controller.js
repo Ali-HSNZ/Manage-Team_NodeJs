decec4fea1f9cd3938a8d7e75d67be87a3d6ea7e
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-08-27 20:54:02 +0430

    Create 'upload Image With Multer' Method | uploadImage_multer | And Create Get User Profile Method | And Get Users List

--- App/Http/Controllers/user.controller.js
+++ App/Http/Controllers/user.controller.js
@@ -1,12 +1,25 @@
 const { UserModel } = require("../../Models/user");
+const pathUpload = require("../../Modules/filePath");
 
 class UserController{
-    getProfile(){
-
-    
+    async getProfile(req,res,next){
+        try {
+            const user = await UserModel.findById(req.user._id)
+            return res.status(200).json(user)
+        } catch (error) {
+            next(error)
+        }
     }
-    async editProfile(req,res,next){
+    async getUserlist(req,res,next){
+        try {
+            const users = await UserModel.find({} , {password : 0 , createdAt : 0 , updatedAt : 0 , __v : 0 , token : 0})
+            return res.status(200).json(users)
+        } catch (error) {
+            next(error)
+        }
+    }
 
+    async editProfile(req,res,next){
         try {
             let body = req.body;
             const userID = req.user._id
@@ -33,22 +46,35 @@ class UserController{
         } catch (error) {
             next(error)
         }
-        
     }
+
     addSkills(){
 
-    
+    }
+    async uploadImage_multer(req,res,next){
+        try {
+           
+            const imagePath = req.protocol+ "://" + req.get('host') +'/'+ req.file.path.substring(7).replace(/[\\\\]/gm , "/")
+
+            const resualt = await UserModel.updateOne({_id : req.user._id} , {$set : {image : imagePath}})
+            if(resualt.modifiedCount <= 0) throw {message : "به روز رسانی انجام نشد" , status : 400 , success : false}
+            return res.status(200).json({
+                status : 200,
+                message : "به روز رسانی تصویر با  موفقیت انجام شد", 
+                success : true
+            })
+
+        } catch (error) {
+            next(error)
+        }
     }
     editSkills(){
 
-    
     }
     acceptInviteTeam(){
-
     
     }
     rejectInviteInTeam(){
-
     
     }
 }
