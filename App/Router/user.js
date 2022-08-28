ce751e3feb2a83130cdabcc18fbad42c5888cbcf
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-08-28 22:00:10 +0430

    Create Upload Profile image Route | Create Get Users List Route | Create Get Profile info Route | Create Edit Profile Route | => App/Router/user.js

--- App/Router/user.js
+++ App/Router/user.js
@@ -1,9 +1,22 @@
 const { UserController } = require('../Http/Controllers/user.controller')
 const {autoLogin} = require('../Http/Middlewares/autoLogin')
+const imageValidator = require('../Http/Validations/user')
+const { upload_multer } = require('../Modules/uploadImage_multer');
 const { expressValidatorMaper } = require('../Http/Middlewares/expressValidatorMaper')
+
 const Router = require('express').Router()
 
-Router.post('/profile' , autoLogin , UserController.editProfile)
+Router.get('/list' ,autoLogin , UserController.getUserlist)
+Router.get('/' ,autoLogin , UserController.getProfile)
+Router.post('/profile' , autoLogin , UserController.editProfile);
+Router.post('/upload-image' ,
+    upload_multer.single('image'),
+    imageValidator(),  
+    expressValidatorMaper,
+    autoLogin,
+    UserController.uploadImage_multer,
+)
+
 
 
 module.exports = {
